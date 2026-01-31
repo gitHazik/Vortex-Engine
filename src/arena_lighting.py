e85e87fd17c55b0d55dc9fd266bb3ebf6a01186d
gitHazik <hazikkhalid700@gmail.com>
2026-01-31 12:24:26 +0530

    init

--- src/arena_lighting.py
+++ src/arena_lighting.py
@@ -5,38 +5,40 @@ from panda3d.core import LPoint3f, Point3, Vec3, Vec4, LVecBase3f, VBase4, LPoin
 def lighting():
     # Ambient light - provides base illumination
     amb_light = AmbientLight('amblight')
-    amb_light.set_color(Vec4(0.3, 0.3, 0.35, 1))  # Reduced from Vec3(1)
+    amb_light.set_color(Vec4(0.5, 0.5, 0.55, 2))  # Increased ambient to rely less on spotlights
     amb_light_node = base.render.attach_new_node(amb_light)
     base.render.set_light(amb_light_node)
 
-    # Main spotlight - reduced intensity
+    # Main spotlight - HEAVILY REDUCED to eliminate white ground
     slight_1 = Spotlight('slight_1')
-    slight_1.set_color(Vec4(2.5, 2.5, 2.0, 1))  # Reduced from 8 to 2.5
+    slight_1.set_color(Vec4(0.3, 0.3, 0.25, 1))  # Very low intensity
     slight_1.set_shadow_caster(True, 4096, 4096)
     lens = PerspectiveLens()
     slight_1.set_lens(lens)
-    slight_1.get_lens().set_fov(120)
+    slight_1.get_lens().set_fov(80)  # 
+    slight_1.get_lens().set_near_far(1, 400)  # Limit range
     slight_1_node = base.render.attach_new_node(slight_1)
     slight_1_node.set_pos(69, -49, 90)
     slight_1_node.look_at(0, 0, 0.5)
     base.render.set_light(slight_1_node)
     
-    # Secondary spotlight - fill light
+    # Secondary spotlight - very subtle fill light
     slight_2 = Spotlight('slight_2')
-    slight_2.set_color(Vec4(0.8, 0.8, 1.0, 1))  # Slightly blue tint
+    slight_2.set_color(Vec4(0.2, 0.2, 0.25, 1))  # Very low, slightly blue
     lens = PerspectiveLens()
     slight_2.set_lens(lens)
-    slight_2.get_lens().set_fov(40)
+    slight_2.get_lens().set_fov(70)
+    slight_2.get_lens().set_near_far(1, 200)
     slight_2_node = base.render.attach_new_node(slight_2)
     slight_2_node.set_pos(-69, -49, 90)
     slight_2_node.look_at(0, 0, 20)
     base.render.set_light(slight_2_node)
 
-    # Environment point light for skybox
+    # Environment point light for skybox only
     env_light_1 = PointLight('env_light_1')
-    env_light_1.set_color(Vec4(1.2, 1.2, 1.0, 1))  # Reduced from 6
+    env_light_1.set_color(Vec4(0.4, 0.4, 0.35, 1))  # Very subtle
     env_light_1_node = base.render.attach_new_node(env_light_1)
-    env_light_1_node.set_pos(0, 0, 0)
+    env_light_1_node.set_pos(0, 0, 100)  # Move up so it doesn't brighten ground
 
     # Load skybox
     base_env = base.loader.load_model('models/daytime_skybox.bam')
@@ -46,7 +48,4 @@ def lighting():
     base_env.set_pos(0, 0, 0)
     base_env.set_light(env_light_1_node)
     base_env.set_light_off(base.render.find('**/slight_1'))
-
-
-
-
+    base_env.set_light_off(base.render.find('**/slight_2'))  
